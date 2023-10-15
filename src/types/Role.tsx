7a930e3069b6e676d5e64bee53d155f29519997d
Eloi Honnet <eloi.honnet@epita.fr>
2023-10-15 19:51:32 +0800

    changed description to be way shorter
    Signed-off-by: Eloi Honnet <eloi.honnet@epita.fr>

--- src/types/Role.tsx
+++ src/types/Role.tsx
@@ -5,93 +5,33 @@ interface Role {
 // const CommonRoles = ["WEREWOLF", "TALKATIVE_WOLF", "BLACK_WOLF", "SEER", "DRUNK_VILLAGER", "WITCH", "LITTLE_GIRL", "HUNTER", "THE_RED_RIDING_HOOD", "GUARD", "CUPID", "DICTATOR"] as const;
 // type CommonRole = typeof CommonRoles[number];
 enum RoleDescription {
-  VILLAGER = `
-  Their objective is to defeat the Werewolves. His word is his only power of persuasion to eliminate them. Stay alert for hints, and identify the culprits.
-  
-  In the case of an equality during the daily vote and if no mayor is elected, no player dies.
-  The votes of the Villagers can be decisive in the fight against the Werewolves.
-`,
-  WEREWOLF = `
-  Defeating the villagers is their objective. During that night, the Werewolves join together and vote on who should be eliminated. During the day, they cannot be unmasked.
-  
-  The Werewolves are called once per night. They can discuss with other Wolves during their voting phase.
-  If the vote leads to a tie, a player dies randomly among those who were targeted in the tie.
-  No player will die if the Wolves don't target anyone.
-  `,
-  TALKATIVE_WOLF = `
-  Defeating the Villagers is his objective. Every day, he needs to say the word "Werewolf" before the sun sets, if he wishes to stay alive.
-  
-  The Talkative Wolf is a Werewolf with an extra constraint.
-  He dies at the end of the day if he has not said his word.
-  `,
-  BLACK_WOLF = `
-  Defeating the villagers is his objective. During the night, he wakes up with all the other wolves. Once during the game, he can turn the chosen victim into a Werewolf.    This is a challenge, because everyone playing will need to actively avoid saying the word “Werewolf” in an attempt to root out the Alpha Werewolf, who must say it at least once.
+  VILLAGER = `Aims to defeat Werewolves through voting. Has no special abilities.`,
 
-  The Black Wolf is a Werewolf with a supplementary power.
-  Once the victim has been chosen by the Werewolves, the Black Wolf has the possibility not to kill that person and to turn him into a Werewolf.
-  The infected player keeps his original role and powers, and adds the Werewolf's characteristics: he wakes up at night with the other wolves to vote, and wins the game by eliminating the entirety of the village.
-  The Seer cannot know that a person has been infected, which makes that player harder to identify and thus stronger.
-  However, the Mentalist and the Gravedigger take in account his wolf identity via their powers.
-  If at sunrise, it has not been proposed to the Black Wolf to infect a player even if no player has been infected yet, then the players can then conclude that there was no vote on the wolves' part, the Guard protected the Wolves' target, or the Red Riding Hood was the target.    
-    `,
-  SEER = `
-  Her goal is to defeat the werewolves. Each night, she can learn the role of a player she has chosen. She must help the villagers without being found out.
-  Use your power each night by clicking on the player of your choice to find out their role.
-  `,
-  DRUNK_VILLAGER = `
-  Defeating the Werewolves is his objective. He can't talk. He can only communicate with gestures or noises.
+  WEREWOLF = `Targets villagers. Votes at night to eliminate one. Must blend in during the day.`,
 
-  This role, while first and foremost taking on all the elements of a regular Villager throughout the game, also has the additional burden of only being able to communicate with gestures or noises.
-  They may not talk during the day at all, and if they do, automatically die during that night.
-  It may be the strategy of others (Werewolves, for instance) to pretend to be the Drunk, as the role is so unique and easily recognized.
-    `,
-  WITCH = `
-  The Witch's objective is to defeat the Werewolves. The Witch wakes up every night and can use one of her two potion : save the Werewolves victim, or kill another player.
+  TALKATIVE_WOLF = `A Werewolf that must say the word "Werewolf" daily to survive.`,
 
-  The Witch can only use one potion every night. She can choose to do nothing.
-  She only has one of each potion.
-  Her potion overrides the Guard's protection.
-    `,
-  LITTLE_GIRL = `
-  Defeating the Werewolves is her objective. She can spy on the werewolfs.
+  BLACK_WOLF = `A Werewolf that can turn a chosen victim into another Werewolf once per game.`,
 
-  She can spy on the Werewolves at night, but she must be careful not to be caught by them.
-    `,
-  HUNTER = `
-  Defeating the Werewolves is his objective. His role has a direct impact on the village. When the Hunter dies he has the power to bring another player with him in his grave.
+  SEER = `Aims to defeat Werewolves. Can identify if a player is a werewolf each night.`,
 
-  The Hunter has the possibility to not kill a player when he dies if he finds that to be strategic.
-    `,
-  THE_RED_RIDING_HOOD = `
-  Her goal is to defeat the Werewolves. As long as the Hunter is alive, she is protected from Werewolf attacks.
+  DRUNK_VILLAGER = `A Villager who can't speak and communicates only through gestures. If he speaks, he dies.`,
 
-  When the wolves target the Red Riding Hood while the Hunter is alive, their vote is not taken into account.
-  If the Hunter dies, then the Red Riding Hood becomes similar to a Villager.
-      `,
-  GUARD = `
-  His objective is to defeat the Werewolves. Every night, he can protect a different player against the Werewolves' attacks.
-  
-  The Guard cannot protect the same person twice in a row
-  He can protect himself.
-  The protected player is still vulnerable to other attacks, such as the Witch's death potion.
-    `,
-  CUPID = `
-  Defeating the Werewolves is his objective. The first night, he designate two lovers. If one of them dies, the other will follow them to their grave.
-  
-  Cupid only has the power to designate the two lovers. Once they've been named, he becomes similar to a Simple Villager.
-  He can auto-designate himself as a lover.
-  The two lovers can be in different party's. If a Wolf is coupled with a Villager, then they need to eliminate all the players to win the game.
-  The two lovers can discuss at any moment through a private chat that you can activate via the button on the left of where you write messages.
-  Copying the text indicating the information on your couple in the chat is considered as unfair gameplay.
-    `,
-  DICTATOR = `
-  His objective is to defeat the Werewolves. He can take over the voting power of the village once in the game. If he executes a Werewolf, he becomes Mayor, otherwise he dies.
+  WITCH = `Can either save a Werewolf's victim or kill a player at night. Has limited potions.`,
 
-  After deciding to take revolution, the Dictator takes the vote at dawn.
-  If he has targeted a Wolf and a Mayor is already elected, he becomes Mayor, if he has targeted a Villager, he dies.
-  The Talkative Wolf does not need to place his word during the day if the Dictator takes power.
-    `,
+  LITTLE_GIRL = `Can spy on Werewolves at night, but risks getting caught.`,
+
+  HUNTER = `Upon death, has the ability to kill another player.`,
+
+  THE_RED_RIDING_HOOD = `Protected from Werewolves as long as the Hunter is alive.`,
+
+  GUARD = `Can protect a player from Werewolves each night but not twice the same player in a row.`,
+
+  CUPID = `Designates two lovers the first night. If one lover dies, so does the other.`,
+
+  DICTATOR = `Can override village vote once. Becomes Mayor if successful, dies if not.`,
 }
+
 enum RoleName {
   VILLAGER = "Villager",
   WEREWOLF = "Werewolf",
